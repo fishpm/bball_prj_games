ed1e620c555430e6b5317175a2f792097da4b758
fishpm <patrick.fisher@gmail.com>
2016-01-25 13:35:12 +0100

    Resolving local/remote version...

--- etl/YearSummary.sql
+++ etl/YearSummary.sql
@@ -9,7 +9,11 @@ DROP TABLE IF EXISTS year_summary;
 CREATE TABLE year_summary (
     last_name VARCHAR(255),
     BB integer,
+    BBfrac float,
+    BBz float,
     HR integer,
+    HRfrac float,
+    HRz float,
     HP integer,
     FC integer,
     id VARCHAR(255),
@@ -20,21 +24,32 @@ CREATE TABLE year_summary (
     IBB integer,
     AB integer,
     D integer,
+    Dfrac float,
+    Dz float,
     H integer,
     K integer,
+    Kfrac float,
+    Kz float,
     team VARCHAR(100),
     S integer,
+    Sfrac float,
+    Sz float,
     T integer,
+    Tfrac float,
+    Tz float,
     SLG float,
+    SLGz float,
     OBP float,
+    OBPz float,
     OPS float,
     SH integer,
     Year date,
     AVG float,
+    AVGz float,
     SF integer
-)
+);
 
 COPY year_summary
-FROM 'YearSummary1980_2014.csv'
+FROM '/Users/patrickfisher/Documents/projects/bball_prj_games/YearSummary1980_2014.csv'
 WITH CSV HEADER
 DELIMITER AS ',';
